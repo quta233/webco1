7cc6c3ee99df0f9dc8cd44f4de888cdb1fe9da28
unknown <liurantina@outlook.com>
2019-12-05 19:24:48 -0500

    build

---
+++ found/build/precache-manifest.af546f0110ae0fd1fe268c90254be948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfffc21f94cc9f3517b3257acfaef52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19d18247c22772bc046",
+    "url": "/static/css/main.8b8ee773.chunk.css"
+  },
+  {
+    "revision": "7c2d211eb215907c7017",
+    "url": "/static/js/2.e04d33cc.chunk.js"
+  },
+  {
+    "revision": "e19d18247c22772bc046",
+    "url": "/static/js/main.2cbd5aa9.chunk.js"
+  },
+  {
+    "revision": "242da622b956264cf1c5",
+    "url": "/static/js/runtime-main.959e9a0c.js"
+  }
+]);
