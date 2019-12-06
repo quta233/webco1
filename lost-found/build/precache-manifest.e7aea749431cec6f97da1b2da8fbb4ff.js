c344bc365b33710370e687b5716ccae4381b4c95
unknown <liurantina@outlook.com>
2019-12-05 19:21:48 -0500

    build

---
+++ lost-found/build/precache-manifest.e7aea749431cec6f97da1b2da8fbb4ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495a6d6482cf50494909d23f649969a7",
+    "url": "/webco1/lost-found/index.html"
+  },
+  {
+    "revision": "aba60c9ddf1945e2c906",
+    "url": "/webco1/lost-found/static/css/main.2a4168ec.chunk.css"
+  },
+  {
+    "revision": "b087ab51f06ea6c9bf19",
+    "url": "/webco1/lost-found/static/js/2.7d355753.chunk.js"
+  },
+  {
+    "revision": "aba60c9ddf1945e2c906",
+    "url": "/webco1/lost-found/static/js/main.c3364730.chunk.js"
+  },
+  {
+    "revision": "2f1abb1e2b71d6220ebf",
+    "url": "/webco1/lost-found/static/js/runtime-main.b0caec5a.js"
+  }
+]);
